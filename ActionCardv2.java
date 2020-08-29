72f90dadffe228917c07e45978393524ac31af27
Gavin Raine Dizon <60138634+gavindizon@users.noreply.github.com>
2020-08-30 00:04:07 +0800

    added javadoc

--- ActionCardv2.java
+++ ActionCardv2.java
@@ -1,9 +1,21 @@
+/**
+ * implements an Action Card that has a description a cash value and a type. It
+ * also has a boolean toAll which sets whether the cash value will be
+ * distributed to all.
+ */
+
 public class ActionCardv2 {
     private String description;
     private boolean toAll;
     private double value;
     private int type;
 
+    /**
+     * Creates an action card object with the specified type
+     * 
+     * @param type type will either be 1 (collect from the bank), 2 (pay the bank),
+     *             3 (Collect from the player), or 4 (pay player).
+     */
     public ActionCardv2(int type) {
         String[] description;
         this.type = type;
@@ -38,17 +50,40 @@ public class ActionCardv2 {
 
     }
 
+    /**
+     * returns the description of the action card
+     * 
+     * @return description
+     */
     public String getDescription() {
         return this.description;
     }
+
+    /**
+     * returns the cash value of the action card
+     * 
+     * @return value
+     */
 
     public double getValue() {
         return this.value;
     }
 
+    /**
+     * returns the type of the action card
+     * 
+     * @return type
+     */
+
     public int getType() {
         return this.type;
     }
+
+    /**
+     * returns whether the boolean toAll of the action card
+     * 
+     * @return toAll
+     */
 
     public boolean getToAll() {
         return this.toAll;
